6f156dd82623d633906ed1d49964dddc91ac0eba
Jozef Cipa <cipa.jozef@gmail.com>
2022-11-04 21:53:46 +0100

    feat: add api

---
+++ src/api.ts
@@ -0,0 +1,39 @@
+const API_URL = 'https://www.cnb.cz/en/financial-markets/foreign-exchange-market/central-bank-exchange-rate-fixing/central-bank-exchange-rate-fixing/daily.txt'
+
+// TODO: fix issue with CORS proxy
+
+export interface Currency {
+    country: string
+    currency: string
+    amount: number
+    code: string
+    rate: number
+}
+
+export const fetchApi = async () => {
+    const res = await fetch(API_URL)
+    return await res.text()
+}
+
+const parseResponse = (response: string): Array<Currency> => {
+    return response
+        .trim()
+        .split('\n')
+        .slice(2) // strip first two rows (headers)
+        .map(currencyRaw => {
+            const [country, currency, amount, code, rate] = currencyRaw.split('|')
+            return {
+                country,
+                currency,
+                amount: Number(amount),
+                code,
+                rate: Number(rate),
+            }
+        })
+}
+
+export const loadExchangeRates = async () => {
+    const response = await fetchApi()
+
+    return parseResponse(response)
+}
